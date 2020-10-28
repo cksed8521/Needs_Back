b9e907e33356a0848747d2454c018b5323c60168
deri <cksed8521@yahoo.com.tw>
2020-10-28 18:29:48 +0800

    not yet to fix upload

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ const {v4: uuidv4} = require('uuid')
 const socketio = require('socket.io')
 const http = require('http')
 const cors = require('cors')
-const axios = require('axios');
+
+const multer = require("multer");
+const upload = multer({ dest: __dirname + "/tmp_uploads" });
 
 const PORT = process.env.PORT || 5000
 
 const router = require('./router')
-
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(router)
 app.use(cors())
 
@@ -28,6 +31,18 @@ app.get("/try-db", (req, res) => {
   });
 });
 
+app.post("/try-uploads", upload.single("img"), (req, res) => {
+  console.log(1);
+  console.log(req.file);
+  console.log(2);
+  res.json(req.file);
+});
+
+app.use("/product", require(__dirname + "/src/productList/productList"));
+app.use("/article", require(__dirname + "/src/article/article"));
+
+
+app.use(express.static(__dirname + "/public"));
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started on port ${PORT}`))
 
