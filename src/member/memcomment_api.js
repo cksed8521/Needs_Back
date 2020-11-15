a977614ec81d403ee20d7ef06c8e46e30dbb0c70
Winnie15951 <69294234+Winnie15951@users.noreply.github.com>
2020-11-15 11:41:59 +0800

    get null comment/try to post new comment

--- src/member/memcomment_api.js
+++ src/member/memcomment_api.js
@@ -4,28 +4,53 @@ const db = require(__dirname + "/../db_connect");
 
 const router = express.Router();
 
-//
+async function Comment(req) {
+  const output = {};
+  switch (req.query.filter) {
+    case "already":
+      let sqlGetcomment =
+        "SELECT order_evaluations.*, products.title,products.image_path,merchants.brand_name FROM order_evaluations LEFT JOIN products ON order_evaluations.product_id = products.id LEFT JOIN merchants ON order_evaluations.merchant_id = merchants.id WHERE customer_id = ? AND order_evaluations.buyer_message IS NOT null";
+      const [results1] = await db.query(sqlGetcomment, [req.query.customer_id]);
+      output.filter = "brands";
+
+      results1.map((product) => {
+        const img = product.image_path.trim().split(",")[0];
+        product.image_path = img;
+      });
+      console.log('aaa')
+      output.rows = results1;
+      return output;
+      
+    case "notyet":
+      let sqlUncomment =
+        "SELECT order_evaluations.*, products.title,products.image_path,merchants.brand_name FROM order_evaluations LEFT JOIN products ON order_evaluations.product_id = products.id LEFT JOIN merchants ON order_evaluations.merchant_id = merchants.id WHERE customer_id = ? AND order_evaluations.buyer_message IS null";
+      const [results2] = await db.query(sqlUncomment, [req.query.customer_id]);
+      output.filter = "notyet";
+      results2.map((product) => {
+        const img = product.image_path.trim().split(",")[0];
+        product.image_path = img;
+      });
+      output.rows = results2;
+      return output;
+  }
+}
 router.get("/", async (req, res) => {
-  // console.log(1)
-  const sql = "SELECT order_evaluations.*, products.title,products.image_path,merchants.brand_name FROM order_evaluations LEFT JOIN products ON order_evaluations.product_id = products.id LEFT JOIN merchants ON order_evaluations.merchant_id = merchants.id WHERE customer_id = ? AND order_evaluations.buyer_message IS NOT null";
-  
-  // console.log('req',req)
-  //query為後端api給資料/params為前端網址列提供id訊息/   為form表單提供資料
- 
-    const [results] = await db.query(sql, [req.query.customer_id]);
-    // console.log(2)
-    results.map((product) => {
-      // console.log(product.image_path)
-      //將資料庫圖片路徑切割
-      const img = product.image_path.trim().split(",")[0]
-      // console.log(img)
-      product.image_path = img
-    })
-    // console.log(3)
+  const output = await Comment(req);
+  console.log('bbb')
+  res.json(output);
+});
 
-
-  if (!results.length) return res.send("NO fund data");
-  res.json(results);
+router.post("/",async(req,res)=>{
+  const data = {...req}
+  console.log('data.body',data.body)
+  const sql = "UPDATE `order_evaluations` SET ? WHERE `order_evaluations`.`id` = ?"
+  const [{affectedRows,changedRows}] = await db.query(sql,[data.body,req.query.id]);
+  console.log('ccc')
+  res.json({
+    success:!!changedRows,
+    affectedRows,
+    changedRows,
+  });
 })
 
 module.exports = router;
