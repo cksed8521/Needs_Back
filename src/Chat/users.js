7c2738bae297577ec2e0906f3a2c37ff07950916
deri <cksed8521@yahoo.com.tw>
2020-11-16 02:21:40 +0800

    update Chat message to MySQL

--- src/Chat/users.js
+++ src/Chat/users.js
@@ -5,11 +5,10 @@ const addUser = ({ id, name, room }) => {
     const existingUser = users.find((user) => user.room === room && user.name === name)
 
     if (!name || !room) return { error: 'Username and room are required' }
-    if (existingUser) return { error: 'Username is taken' }
 
     const user = { id, name, room }
-
     users.push(user)
+
 
     return { user }
 }
@@ -24,4 +23,4 @@ const getUser = (id) => users.find((user) => user.id === id)
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room)
 
-module.exports = { addUser, removeUser, getUser, getUsersInRoom }
+module.exports = { addUser, removeUser, getUser, getUsersInRoom, users }
