2760181aeacce0da56b6f51fe9b8a5f23f92037b
bookmountain <bookmountain0222@gmail.com>
2020-11-16 23:01:45 +0800

    merchant to 12

--- src/Backend/Dashboard/route.js
+++ src/Backend/Dashboard/route.js
@@ -121,31 +121,31 @@ router.get("/incomelastweek", (req, res) => {
 });
 
 router.get("/deliverytablefordashboard", (req, res) => {
-  db.query("SELECT *, order_deliveries.status AS delivery_status, orders.status AS status FROM orders JOIN order_deliveries ON orders.delivery_id = order_deliveries.id WHERE merchant_id = 3 ORDER BY orders.created_at DESC LIMIT 7").then(([result]) => {
+  db.query("SELECT *, order_deliveries.status AS delivery_status, orders.status AS status FROM orders JOIN order_deliveries ON orders.delivery_id = order_deliveries.id WHERE merchant_id = 12 ORDER BY orders.created_at DESC LIMIT 7").then(([result]) => {
     res.json(result);
   });
 });
 
 router.get("/deliverystatusamount", (req, res) => {
-  db.query("SELECT order_deliveries.status AS delivery_status FROM orders JOIN order_deliveries ON orders.delivery_id = order_deliveries.id WHERE merchant_id = 3 ORDER BY orders.created_at DESC").then(([result]) => {
+  db.query("SELECT order_deliveries.status AS delivery_status FROM orders JOIN order_deliveries ON orders.delivery_id = order_deliveries.id WHERE merchant_id = 12 ORDER BY orders.created_at DESC").then(([result]) => {
     res.json(result);
   });
 });
 
 router.get("/amountoforders", (req, res) => {
-  db.query("SELECT * FROM orders WHERE merchant_id = 3 ORDER BY orders.created_at DESC").then(([result]) => {
+  db.query("SELECT * FROM orders WHERE merchant_id = 12 ORDER BY orders.created_at DESC").then(([result]) => {
     res.json(result);
   });
 });
 
 router.get("/merchantsellrank", (req, res) => {
-  db.query("SELECT *, count(1) total_order_amount, SUM(quantity) total_quantity FROM `products` INNER JOIN product_skus ON products.id = product_skus.product_id INNER JOIN order_products ON product_skus.id = order_products.product_sku_id INNER JOIN product_categories ON product_categories.id = products.categories_id WHERE merchant_id = 3 GROUP BY product_id ORDER BY total_quantity DESC").then(([result]) => {
+  db.query("SELECT *, count(1) total_order_amount, SUM(quantity) total_quantity FROM `products` INNER JOIN product_skus ON products.id = product_skus.product_id INNER JOIN order_products ON product_skus.id = order_products.product_sku_id INNER JOIN product_categories ON product_categories.id = products.categories_id WHERE merchant_id = 12 GROUP BY product_id ORDER BY total_quantity DESC LIMIT 5").then(([result]) => {
     res.json(result);
   });
 });
 
 router.get("/merchantsellrankgroupbyname", (req, res) => {
-  db.query("SELECT *, count(1) total_order_amount, SUM(quantity) total_quantity FROM `products` INNER JOIN product_skus ON products.id = product_skus.product_id INNER JOIN order_products ON product_skus.id = order_products.product_sku_id INNER JOIN product_categories ON product_categories.id = products.categories_id WHERE merchant_id = 3 GROUP BY name ORDER BY total_quantity DESC").then(([result]) => {
+  db.query("SELECT *, count(1) total_order_amount, SUM(quantity) total_quantity FROM `products` INNER JOIN product_skus ON products.id = product_skus.product_id INNER JOIN order_products ON product_skus.id = order_products.product_sku_id INNER JOIN product_categories ON product_categories.id = products.categories_id WHERE merchant_id = 12 GROUP BY name ORDER BY total_quantity DESC LIMIT 5").then(([result]) => {
     res.json(result);
   });
 });
