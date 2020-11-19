fb8d566d52c2c2921cac71c1968d2322efc15180
calico-elivia <69293713+calico-elivia@users.noreply.github.com>
2020-11-19 16:38:23 +0800

    reset api url

--- src/article/article.js
+++ src/article/article.js
@@ -1,26 +1,25 @@
-const express = require("express");
+const express = require('express')
 
-const db = require(__dirname + "/../db_connect");
-const router = express.Router();
-const upload = require(__dirname + '/articleUploadModule');
+const db = require(__dirname + '/../db_connect')
+const router = express.Router()
+const upload = require(__dirname + '/articleUploadModule')
 
 //email
-const sendMail = require(__dirname+"/mail")
-
+const sendMail = require(__dirname + '/mail')
 
 //success to upload into EditorJS
-router.post('/fetchUrl', upload.single("image") ,(req,res) => {
-  const imgPath = "http://localhost:5000/articleImg/" + req.file.filename
-    res.json({
-      "success" : 1,
-      "file": {
-          "url" : imgPath,
-          // ... and any additional fields you want to store, such as width, height, color, extension, etc
-      }
+router.post('/fetchUrl', upload.single('image'), (req, res) => {
+  const imgPath = 'http://122.116.38.12:5050/articleImg/' + req.file.filename
+  res.json({
+    success: 1,
+    file: {
+      url: imgPath,
+      // ... and any additional fields you want to store, such as width, height, color, extension, etc
+    },
   })
 })
 
-  //可以成功上傳的內容
+//可以成功上傳的內容
 // router.post("/upload", upload.single("image") , async(req, res) =>{
 // res.json(req.file)
 // const imgPath = "localhost:5000/" + req.file.filename
@@ -36,13 +35,15 @@ router.post('/fetchUrl', upload.single("image") ,(req,res) => {
 // })
 
 // get all article
-router.get("/", (req, res) => {
-  db.query("SELECT * FROM `article` ORDER BY `article`.`id` DESC").then(([results])=>{
-    res.json(results)
-  })
-});
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM `article` ORDER BY `article`.`id` DESC').then(
+    ([results]) => {
+      res.json(results)
+    }
+  )
+})
 
-router.post('/',upload.single("image"),async(req, res) => {
+router.post('/', upload.single('image'), async (req, res) => {
   console.log(req.body)
   const setTitle = req.body[0]
   const imgPath = req.body[1]
@@ -50,60 +51,57 @@ router.post('/',upload.single("image"),async(req, res) => {
   const setDetial = req.body[3]
 
   const sql =
-    "INSERT INTO `article`(`title`, `image`, `outline`, `detial`,create_at) VALUES (?,?,?,?,now())";
-  const [{ affectedRows, insertId }] = await db.query(sql, [setTitle,imgPath,setOutline,setDetial]);
+    'INSERT INTO `article`(`title`, `image`, `outline`, `detial`,create_at) VALUES (?,?,?,?,now())'
+  const [{ affectedRows, insertId }] = await db.query(sql, [
+    setTitle,
+    imgPath,
+    setOutline,
+    setDetial,
+  ])
 
   res.json({
     success: !!affectedRows,
     affectedRows,
     insertId,
-  });
-});
-
-
-
+  })
+})
 
 async function getArticle(id) {
-  const article_sql =
-  'SELECT * FROM `article` WHERE article.id=?'
-  const [[product]] = await db.query(article_sql, [id]);
+  const article_sql = 'SELECT * FROM `article` WHERE article.id=?'
+  const [[product]] = await db.query(article_sql, [id])
 
-  return product;
+  return product
 }
 
-router.get("/:id", async (req, res) => {
-  res.json(await getArticle(req.params.id));
-});
+router.get('/:id', async (req, res) => {
+  res.json(await getArticle(req.params.id))
+})
 
 // delete article
 
-router.delete("/:id" ,async(req, res) => {
+router.delete('/:id', async (req, res) => {
   console.log(req.params.id)
-  const sql =  "DELETE FROM `article` WHERE id=?"
+  const sql = 'DELETE FROM `article` WHERE id=?'
   const [results] = await db.query(sql, [req.params.id])
   res.json(results)
 })
 
-
 // send email (email, title , text)
-router.post('/email', (req, res) =>{
+router.post('/email', (req, res) => {
   const subject = req.body[0]
   const image = req.body[1]
   const html = req.body[2].__html
-  console.log('subject' , req.body[0])
-  console.log('image' , req.body[1])
-  console.log('html' , req.body[2].__html)
-  
-  sendMail(subject,image ,html , function(err,data){
-    if(err){
-      res.status(500).json({message:'Internal Error'})
+  console.log('subject', req.body[0])
+  console.log('image', req.body[1])
+  console.log('html', req.body[2].__html)
+
+  sendMail(subject, image, html, function (err, data) {
+    if (err) {
+      res.status(500).json({ message: 'Internal Error' })
     } else {
-      res.json({message:'Email Send'})
+      res.json({ message: 'Email Send' })
     }
   })
 })
 
-
-
-
-module.exports = router;
+module.exports = router
